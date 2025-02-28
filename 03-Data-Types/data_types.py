5ff9f413b17d3c9f893c478c98c3db6d887d3d1f
hshehriyaralam <hshehriyaralam@gmail.com>
2025-03-01 00:32:57 +0500

    data-types

---
+++ 03-Data-Types/data_types.py
@@ -0,0 +1,42 @@
+
+
+#int
+a = 5
+print(type(a))
+print(a)
+
+#float
+b = 3.2
+print(type(b))
+print(b)
+
+
+# str
+c = 'hello world'
+print(type(c))
+print(c)
+
+
+#complex
+d = 6 + 9j
+e = 2 + 5j
+print(d + e)
+
+
+#list
+f = [10,20,30,"shsh"]
+print(type(f))
+print(f)
+
+#tuple
+g = "b", "9", "y", "9"
+print(type(g))
+print(g)
+
+
+#set
+h = {2,3,4,5,5,6,6,6,2,}
+print(type(h))
+print(h)
+
+
