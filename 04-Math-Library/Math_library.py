63f4c1ab566cae67cef2d7db77d355696cbafc15
hshehriyaralam <hshehriyaralam@gmail.com>
2025-03-01 11:41:53 +0500

    math-commit-reserved

---
+++ 04-Math-Library/Math_library.py
@@ -0,0 +1,41 @@
+import math
+import turtle
+
+# sh = math.factorial(4)
+# print(sh)
+# sl = math.log(9989729)
+# print(sl)
+# si = math.pow(89)
+# print(si)
+# sh = math.lcm(9,4)
+# print(sh)
+
+
+# z=turtle.Screen()
+# z.setup(200,200)
+
+
+a = input()
+b= input()
+print("a value",a)
+print("b value",b)
+print(a,b, sep=" and ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''print("comment in Python")'''
---
+++ 04-Math-Library/Reserved_key.py
@@ -0,0 +1,11 @@
+import keyword
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+
+
+#type Conversion 
+x = "20"
+print(type(x))
+z = int(x)
+print(type(z))
+
